3ae5427da00b5dff839fe067bdc3b728881a7e51
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-12-07 15:38:43 +0100

    Idle waits - condition_variables

--- _exercises/synchronization/bank_account.cpp
+++ _exercises/synchronization/bank_account.cpp
@@ -1,156 +1,157 @@
-// #include <iostream>
-// #include <thread>
-// #include <mutex>
+#include <iostream>
+#include <thread>
+#include <mutex>
 
-// class BankAccount
-// {
-//     const int id_;
-//     double balance_;
-//     mutable std::recursive_mutex mtx_balance_;
+class BankAccount
+{
+    const int id_;
+    double balance_;
+    mutable std::recursive_mutex mtx_balance_;
 
-// public:
-//     BankAccount(int id, double balance)
-//         : id_(id)
-//         , balance_(balance)
-//     {
-//     }
+public:
+    BankAccount(int id, double balance)
+        : id_(id)
+        , balance_(balance)
+    {
+    }
 
-//     void print() const
-//     {
-//         double current_balance; 
-//         {
-//             std::lock_guard lk{mtx_balance_};
-//             current_balance = this->balance_;
-//         }
-//         std::cout << "Bank Account #" << id_ << "; Balance = " << current_balance << std::endl;
+    void print() const
+    {
+        double current_balance; 
+        {
+            std::lock_guard lk{mtx_balance_};
+            current_balance = this->balance_;
+        }
+        std::cout << "Bank Account #" << id_ << "; Balance = " << current_balance << std::endl;
 
-//         //std::cout << "Bank Account #" << id_ << "; Balance = " << balance() << std::endl;
-//     }
+        //std::cout << "Bank Account #" << id_ << "; Balance = " << balance() << std::endl;
+    }
 
-//     void transfer(BankAccount& to, double amount)
-//     {
-//         // std::lock(mtx_balance_, to.mtx_balance_);
-//         // std::lock_guard lock1{mtx_balance_, std::adopt_lock};
-//         // std::lock_guard lock2{to.mtx_balance_, std::adopt_lock};
+    void transfer(BankAccount& to, double amount)
+    {
+        // std::lock(mtx_balance_, to.mtx_balance_);
+        // std::lock_guard lock1{mtx_balance_, std::adopt_lock};
+        // std::lock_guard lock2{to.mtx_balance_, std::adopt_lock};
 
-//         // std::unique_lock<std::mutex> lk_from(mtx_balance_, std::defer_lock);
-//         // std::unique_lock<std::mutex> lk_to(to.mtx_balance_, std::defer_lock);
-//         // std::lock(lk_from, lk_to);
+        // std::unique_lock<std::mutex> lk_from(mtx_balance_, std::defer_lock);
+        // std::unique_lock<std::mutex> lk_to(to.mtx_balance_, std::defer_lock);
+        // std::lock(lk_from, lk_to);
 
-//         // balance_ -= amount;
-//         // to.balance_ += amount;
+        // balance_ -= amount;
+        // to.balance_ += amount;
         
-//         std::scoped_lock lks{mtx_balance_, to.mtx_balance_};
-//         withdraw(amount);
-//         to.deposit(amount);
-//     }
+        std::scoped_lock lks{mtx_balance_, to.mtx_balance_};
+        withdraw(amount);
+        to.deposit(amount);
+    }
 
-//     void withdraw(double amount)
-//     {
-//         std::lock_guard lk{mtx_balance_};
-//         balance_ -= amount;
-//     }
+    void withdraw(double amount)
+    {
+        std::lock_guard lk{mtx_balance_};
+        balance_ -= amount;
+    }
 
-//     void deposit(double amount)
-//     {
-//         std::lock_guard lk{mtx_balance_};
-//         balance_ += amount;
-//     }
+    void deposit(double amount)
+    {
+        std::lock_guard lk{mtx_balance_};
+        balance_ += amount;
+    }
 
-//     int id() const
-//     {
-//         return id_;
-//     }
+    int id() const
+    {
+        return id_;
+    }
 
-//     double balance() const
-//     {
-//         std::lock_guard lk{mtx_balance_};
-//         return balance_;
-//     }
+    double balance() const
+    {
+        std::lock_guard lk{mtx_balance_};
+        return balance_;
+    }
 
-//     void lock() 
-//     {
-//         mtx_balance_.lock();
-//     }
+    void lock() 
+    {
+        mtx_balance_.lock();
+    }
 
-//     void unlock()
-//     {
-//         mtx_balance_.unlock();
-//     }
+    void unlock()
+    {
+        mtx_balance_.unlock();
+    }
 
-//     bool try_lock()
-//     {
-//         return mtx_balance_.try_lock();
-//     }
+    bool try_lock()
+    {
+        return mtx_balance_.try_lock();
+    }
 
-//     auto with_lock() const
-//     {
-//         return std::unique_lock{mtx_balance_};
-//     }
-// };
+    auto with_lock() const
+    {
+        return std::unique_lock{mtx_balance_};
+    }
+};
 
-// void make_withdraws(BankAccount& ba, int no_of_operations)
-// {
-//     for (int i = 0; i < no_of_operations; ++i)
-//         ba.withdraw(1.0);
-// }
+void make_withdraws(BankAccount& ba, int no_of_operations)
+{
+    for (int i = 0; i < no_of_operations; ++i)
+        ba.withdraw(1.0);
+}
 
-// void make_deposits(BankAccount& ba, int no_of_operations)
-// {
-//     for (int i = 0; i < no_of_operations; ++i)
-//         ba.deposit(1.0);
-// }
+void make_deposits(BankAccount& ba, int no_of_operations)
+{
+    for (int i = 0; i < no_of_operations; ++i)
+        ba.deposit(1.0);
+}
 
-// void make_transfers(BankAccount& from, BankAccount& to, int no_of_operations)
-// {
-//     for (int i = 0; i < no_of_operations; ++i)
-//         from.transfer(to, 1.0);
-// }
+void make_transfers(BankAccount& from, BankAccount& to, int no_of_operations)
+{
+    for (int i = 0; i < no_of_operations; ++i)
+        from.transfer(to, 1.0);
+}
 
-// void use(const BankAccount& ba)
-// {
-//     auto lk = ba.with_lock();
-//     std::cout << ba.balance() << "\n";
-//     //...
-//     std::cout << ba.balance() << "\n";
-// }
+void use(const BankAccount& ba)
+{
+    auto lk = ba.with_lock();
+    std::cout << ba.balance() << "\n";
+    //...
+    std::cout << ba.balance() << "\n";
+}
 
-// int main()
-// {
-//     const int NO_OF_ITERS = 10'000'000;
+int main()
+{
+    const int NO_OF_ITERS = 10'000'000;
 
-//     BankAccount ba1(1, 10'000);
-//     BankAccount ba2(2, 10'000);
+    BankAccount ba1(1, 10'000);
+    BankAccount ba2(2, 10'000);
 
-//     std::cout << "Before threads are started: ";
-//     ba1.print();
-//     ba2.print();
+    std::cout << "Before threads are started: ";
+    ba1.print();
+    ba2.print();
 
-//     std::thread thd1(&make_withdraws, std::ref(ba1), NO_OF_ITERS);
-//     std::thread thd2(&make_deposits, std::ref(ba1), NO_OF_ITERS);
-//     std::thread thd3(&make_transfers, std::ref(ba1), std::ref(ba2), NO_OF_ITERS);
-//     std::thread thd4(&make_transfers, std::ref(ba2), std::ref(ba1), NO_OF_ITERS);
+    std::thread thd1(&make_withdraws, std::ref(ba1), NO_OF_ITERS);
+    std::thread thd2(&make_deposits, std::ref(ba1), NO_OF_ITERS);
+    std::thread thd3(&make_transfers, std::ref(ba1), std::ref(ba2), NO_OF_ITERS);
+    std::thread thd4(&make_transfers, std::ref(ba2), std::ref(ba1), NO_OF_ITERS);
 
-//     {  // transaction
-//         std::scoped_lock trans_lock{ba1, ba2};
-//         ba1.deposit(1'000);
-//         ba1.transfer(ba2, 500);
-//         ba1.transfer(ba2, 100);
-//     }
+    {  // transaction
+        int x = 10; 
+        std::scoped_lock trans_lock{ba1, ba2}; // lock - acquire
+        ba1.deposit(1'000);
+        ba1.transfer(ba2, 500);
+        ba1.transfer(ba2, 100);
+    } // unlock - release
 
-//     {
-//         auto lk = ba1.with_lock(); // starts transaction
-//         ba1.deposit(100);
-//         ba1.deposit(100);
-//         ba1.withdraw(50);
-//     }
+    {
+        auto lk = ba1.with_lock(); // starts transaction
+        ba1.deposit(100);
+        ba1.deposit(100);
+        ba1.withdraw(50);
+    }
 
-//     thd1.join();
-//     thd2.join();
-//     thd3.join();
-//     thd4.join();
+    thd1.join();
+    thd2.join();
+    thd3.join();
+    thd4.join();
 
-//     std::cout << "After all threads are done: ";
-//     ba1.print();
-//     ba2.print();
-// }
+    std::cout << "After all threads are done: ";
+    ba1.print();
+    ba2.print();
+}
--- event-synchronization/main.cpp
+++ event-synchronization/main.cpp
@@ -1,58 +1,112 @@
+#include <atomic>
 #include <cassert>
 #include <chrono>
+#include <condition_variable>
 #include <functional>
 #include <iostream>
+#include <mutex>
+#include <numeric>
 #include <random>
 #include <string>
 #include <thread>
 #include <vector>
-#include <numeric>
-#include <atomic>
 
 using namespace std::literals;
 
-class Data
+namespace BusyWaits
 {
-    std::vector<int> data_;
-    std::atomic<bool> data_ready_{false};
 
-public:
-    void read()
+    class Data
     {
-        std::cout << "Start reading..." << std::endl;
-        data_.resize(100);
+        std::vector<int> data_;
+        std::atomic<bool> data_ready_ {false};
 
-        std::random_device rnd;
-        std::generate(begin(data_), end(data_), [&rnd]
-            { return rnd() % 1000; });
-        std::this_thread::sleep_for(2s);
-        std::cout << "End reading..." << std::endl;        
-        
-        data_ready_.store(true, std::memory_order_release);
-        //data_ready_ = true; // -> data_ready_.store(true, std::memory_order_seq_cst); XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    }
+    public:
+        void read()
+        {
+            std::cout << "Start reading..." << std::endl;
+            data_.resize(100);
 
-    void process(int id)
-    {       
-        while(!data_ready_.load(std::memory_order_acquire)) // while(!data_ready_.load(std::memory_order_seq_cst)) XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-        {}
-     
-        long sum = std::accumulate(begin(data_), end(data_), 0L);
+            std::random_device rnd;
+            std::generate(begin(data_), end(data_), [&rnd]
+                { return rnd() % 1000; });
+            std::this_thread::sleep_for(2s);
+            std::cout << "End reading..." << std::endl;
 
-        std::cout << "Id: " << id << "; Sum: " << sum << std::endl;
-    }
-};
+            data_ready_.store(true, std::memory_order_release);
+            // data_ready_ = true; // -> data_ready_.store(true, std::memory_order_seq_cst); XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+        }
+
+        void process(int id)
+        {
+            while (!data_ready_.load(std::memory_order_acquire)) // while(!data_ready_.load(std::memory_order_seq_cst)) XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            {
+            }
+
+            long sum = std::accumulate(begin(data_), end(data_), 0L);
+
+            std::cout << "Id: " << id << "; Sum: " << sum << std::endl;
+        }
+    };
+}
+
+namespace IdleWaits
+{
+    class Data
+    {
+        std::vector<int> data_;
+        bool data_ready_ {false};
+        std::mutex mtx_data_ready_;
+        std::condition_variable cv_data_ready_;
+
+    public:
+        void read()
+        {
+            std::cout << "Start reading..." << std::endl;
+            data_.resize(100);
+
+            std::random_device rnd;
+            std::generate(begin(data_), end(data_), [&rnd]
+                { return rnd() % 1000; });
+            std::this_thread::sleep_for(2s);
+            std::cout << "End reading..." << std::endl;
+
+            {
+                std::lock_guard lk {mtx_data_ready_};
+                data_ready_ = true;
+            }
+            cv_data_ready_.notify_all();
+        }
+
+        void process(int id)
+        {
+            std::unique_lock lk {mtx_data_ready_};
+            // while (!data_ready_)
+            // {
+            //     // SC
+            //     cv_data_ready_.wait(lk);
+            // }
+            cv_data_ready_.wait(lk, [this]
+                { return data_ready_; });
+            lk.unlock();
+
+            long sum = std::accumulate(begin(data_), end(data_), 0L);
+
+            std::cout << "Id: " << id << "; Sum: " << sum << std::endl;
+        }
+    };
+}
 
 int main()
 {
     std::cout << "Main thread starts..." << std::endl;
     const std::string text = "Hello Threads";
 
-    Data data;
+    IdleWaits::Data data;
 
     std::thread thd_producer {[&data]
         { data.read(); }};
-    
+
     std::thread thd_consumer1 {[&data]
         { data.process(1); }};
     std::thread thd_consumer2 {[&data]
